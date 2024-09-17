3a03a3e30088712d22b534dccfca02ef961fe922
Zinny92 <zinphuehlaing@gmail.com>
2024-09-17 15:44:07 +0700

    my commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//counter program
+const decreaseBtn = document.getElementById("decreaseBtn");
+
+const resetBtn = document.getElementById("resetBtn");
+
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countlabel");
+ 
+let count = 0;
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 1;
+    countLabel.textContent = count;
+}
